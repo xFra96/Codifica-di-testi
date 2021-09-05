38e0dba05e92af96cffd130b7f8e56f48c65aab8
xFra96 <f.spagnoli1996@gmail.com>
2021-09-06 00:44:01 +0200

    HTML final

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,16 @@
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
+  return new bootstrap.Popover(popoverTriggerEl)
 })
 
 const wait = (delay = 0) =>
   new Promise(resolve => setTimeout(resolve, delay));
 
-const setVisible = (elementOrSelector, visible) => 
+const setVisible = (elementOrSelector, visible) =>
   (typeof elementOrSelector === 'string'
     ? document.querySelector(elementOrSelector)
     : elementOrSelector
   ).style.display = visible ? 'block' : 'none';
-
 setVisible('.page', false);
 setVisible('#loading', true);
 
@@ -19,4 +18,19 @@ document.addEventListener('DOMContentLoaded', () =>
   wait(300).then(() => {
     setVisible('.page', true);
     setVisible('#loading', false);
-  }));
+  })
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const togglers = document.querySelectorAll('.toggler')
+  for (let i = 0; i < togglers.length; i++) {
+    togglers[i].addEventListener("click", function () {
+      let divtoshow = togglers[i].attributes.getNamedItem("data-section").value
+      setVisible('#cart17', false);
+      setVisible('#cart18', false);
+      setVisible('#cart39', false);
+      setVisible('#home', false);
+      setVisible('#' + divtoshow, true);
+    });
+  }
+});
